e9239301d20a12dbd50dee69bf0f622d38569cf9
gamanedy144 <gamanedy@yahoo.com>
2022-06-30 13:31:23 +0300

    Create NodeJS server

---
+++ backend/data.js
@@ -0,0 +1,63 @@
+// dummy data for the database models
+const data = {
+  products: [
+    {
+      //first object
+      _id: 1,
+      name: 'Jam Mountain Bike',
+      slug: 'jam-mountain-bike',
+      category: 'Mountain Bike',
+      image: '/images/p1.png',
+      price: 1,
+      countInStock: 1,
+      brand: 'Jam',
+      rating: 3.5,
+      numReviews: 15,
+      description: 'High quality mountain bike',
+    },
+    {
+      //second object
+      _id: 2,
+      name: 'Test Road Bike',
+      slug: 'test-road-bike',
+      category: 'Road Bike',
+      image: '/images/p2.png',
+      price: 1,
+      countInStock: 1,
+      brand: 'Test',
+      rating: 1,
+      numReviews: 1,
+      description: 'High quality road bike',
+    },
+    {
+      //third object
+      _id: 3,
+      name: 'Carrera Folding Bike',
+      slug: 'carrera-folding-bike',
+      category: 'Folding Bike',
+      image: '/images/p3.png',
+      price: 1,
+      countInStock: 1,
+      brand: 'Carrera',
+      rating: 4.5,
+      numReviews: 5,
+      description: 'High quality folding bike',
+    },
+    {
+      //fourth object
+      _id: 4,
+      name: 'Xiaomi Folding Bike',
+      slug: 'xiaomi-folding-bike',
+      category: 'Folding Bike',
+      image: '/images/p4.png',
+      price: 1,
+      countInStock: 1,
+      brand: 'Xiaomi',
+      rating: 2.5,
+      numReviews: 300,
+      description: 'High quality folding bike',
+    },
+  ],
+};
+
+export default data;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import data from '../data';
 import { Link } from 'react-router-dom';
-
+import backgroundPhoto from '../images/home2.jpg';
 export default function HomeScreen() {
   return (
     <div>
-      <h1>Featured Products</h1>
+      {/* <h1>Featured Products</h1> */}
+      <div className="homescreen-bg-container">
+        <img
+          src={backgroundPhoto}
+          alt="bycicle-store"
+          className="homescreen-bg"
+        />
+      </div>
+
       <div className="products">
         {data.products.map((product) => (
           <div key={product._id} className="product">
