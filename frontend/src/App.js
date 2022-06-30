f543e053e0f83090c8d48afa5e013b6f751d86b0
gamanedy144 <gamanedy@yahoo.com>
2022-06-30 11:14:39 +0300

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Bike'N'Go</a>
+      </header>
+      <main>starting of</main>
+    </div>
+  );
+}
+
+export default App;
