32d4ac3c3fbe6d85057b2f7b7f1075c605902207
gamanedy144 <gamanedy@yahoo.com>
2022-06-30 19:29:59 +0300

    Create utils.js for getError function

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,6 +6,7 @@ import Rating from '../components/Rating';
 import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { getError } from '../utils.js';
 function reducer(state, action) {
   switch (action.type) {
     case 'FETCH_REQUEST':
@@ -33,7 +34,7 @@ export default function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
     fetchData();
