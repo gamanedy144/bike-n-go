d3f67f5391b212826d204192bfa701933d3a1c60
gamanedy144 <gamanedy@yahoo.com>
2022-07-01 16:45:03 +0300

    Update DB path

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI_LOCAL)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('connected to db');
   })
